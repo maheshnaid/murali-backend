942b1c8596a941b2f620616884c78b08c64f9854
Murali <muraliaddakatla@gmail.com>
2026-03-26 16:34:13 +0530

    resolve error

--- routes/register.js
+++ routes/register.js
@@ -4,39 +4,33 @@ import DB from '../database/databaseconnection.js'
 
 const router = express.Router()
 
-// router.post('/', async (req, res) => {
-//     const { username, email, password } = req.body
-//     const database = await DB()
-//     const isUserExists = 'SELECT * FROM users WHERE email = ?'
-//     const [allUsers] = await database.query(isUserExists, email)
-//     const errors = {email_error : '', password_error : ''}
-//     if(allUsers.length > 0){
-//         errors.email_error = 'Email Already Exist'
-//         return res.status(500).json({errors : errors})
-//     }
+router.post('/', async (req, res) => {
+    const { username, email, password } = req.body
+    const database = await DB()
+    const isUserExists = 'SELECT * FROM users WHERE email = ?'
+    const [allUsers] = await database.query(isUserExists, [email])
+    const errors = {email_error : '', password_error : ''}
+    if(allUsers.length > 0){
+        errors.email_error = 'Email Already Exist'
+        return res.status(500).json({errors : errors})
+    }
 
-//     const isValidEmail = email.includes('@gmail.com')
+    const isValidEmail = email.includes('@gmail.com')
 
-//     if(!isValidEmail){
-//         errors.email_error = 'Invalid Emai'
-//         return res.status(500).json({errors : errors})
-//     }else{
-//         if(password.length < 6){
-//             errors.password_error = 'Password too short'
-//             return res.status(500).json({errors : errors})
-//         }else{
-//             const hashedPassword = await bcrypt.hash(password, 10)
-//             const inserQuery = 'INSERT INTO users(username, email, userpassword) VALUES(?, ?, ?)'
-//             await database.query(inserQuery, [username, email, hashedPassword])
-//             res.status(200).json({message : 'user registered successfully'})
-//         }
-//     }
-// })
+    if(!isValidEmail){
+        errors.email_error = 'Invalid Emai'
+        return res.status(500).json({errors : errors})
+    }else{
+        if(password.length < 6){
+            errors.password_error = 'Password too short'
+            return res.status(500).json({errors : errors})
+        }else{
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const inserQuery = 'INSERT INTO users(username, email, userpassword) VALUES(?, ?, ?)'
+            await database.query(inserQuery, [username, email, hashedPassword])
+            res.status(200).json({message : 'user registered successfully'})
+        }
+    }
+})
 
-// export default router
-
-router.post('/', (req, res) => {
-  res.send("Register API working ✅");
-});
-
-export default router;
+export default router
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import profileRoute from './routes/profile.js'
 import registerRoute from './routes/register.js'
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
