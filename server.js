3f442ef795d2a54b81b449e75bc1ec91cc5aca8a
Murali <muraliaddakatla@gmail.com>
2026-03-26 12:48:12 +0530

    root route issue

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-import express, { json } from 'express'
+import express from 'express'
 import cors from 'cors'
 import loginRoute from './routes/login.js'
 import profileRoute from './routes/profile.js'
 import registerRoute from './routes/register.js'
 
-
-
 const app = express();
-app.use(express.json())
-app.use(cors())
+
+app.use(express.json());
+app.use(cors());
 
 
-///User Register API
 // app.post('/register', registerRoute)
 
 
-///User Login API
 // app.post('/login', loginRoute)
 
 
-///Get User Profile API
 // app.get('/profile', profileRoute)
 
 
